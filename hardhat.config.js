7c370960fae7e2fc2023ff4410ee748db45a6ef8
karthik <aedumbakakarthik1@gmail.com>
2023-06-24 22:26:54 +0530

    first NFT

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    // Define the sepolia network parameters here
+    sepolia: {
+      url: process.env.QUICKNODE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
